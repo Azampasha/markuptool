5346cfbed9f9024c7309a0ffb36752e6995a4b3f
AZAM <AZAM@AZAM-HP>
2016-06-28 20:31:39 +0530

    hfh

--- Lab40/src/com/jlcindia/spring/Lab40.java
+++ Lab40/src/com/jlcindia/spring/Lab40.java
@@ -8,7 +8,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Lab40 {
 
 	public static void main(String[] args) {
-		
+	// developer2 comment
 	ApplicationContext ctx=new ClassPathXmlApplicationContext("jlcindia.xml");
 		TestBean tb=(TestBean)ctx.getBean("testBean");
 		tb.showEnglish();
